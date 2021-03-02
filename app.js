0ded434298d3ee3337b87580b87b69378aef5620
vakumar0 <kumarvaibhav1602@gmail.com>
2021-03-02 11:55:49 +0530

    Fixed favicon error

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(expressLayouts);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.get('/favicon.ico', (req, res) => res.status(204));
 
 app.use(function(req, res, next) {
   next(createError(404));
